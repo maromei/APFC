f389feac34a38b4bf3053a2c73b140e11f775aee
maromei <max@pop-os.localdomain>
2023-01-07 22:04:52 +0100

    fixed issue with index of read file fnc

--- src/python/sim/eta_builder.py
+++ src/python/sim/eta_builder.py
@@ -18,6 +18,6 @@ def single_grain(xm: np.array, ym: np.array, config: dict, eta_i: int) -> np.arr
 def load_from_file(xm, ym, config, eta_i):
 
     out_path = f"{config['sim_path']}/out_{eta_i}.txt"
-    eta = rw.read_eta_cplx_last_file(out_path, xm.shape[0], xm.shape[1])
+    eta, _ = rw.read_eta_cplx_last_file(out_path, xm.shape[0], xm.shape[1])
 
     return eta
