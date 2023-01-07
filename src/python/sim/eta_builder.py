2648aa41de7749ab1754795106ef3cae61c530bc
maromei <max@pop-os.localdomain>
2023-01-07 21:29:34 +0100

    added eta_i to builder call

--- src/python/sim/eta_builder.py
+++ src/python/sim/eta_builder.py
@@ -5,7 +5,7 @@ def tanhmin(radius: np.array, eps: float) -> np.array:
     return 0.5 * (1.0 + np.tanh(-3.0 * radius / eps))
 
 
-def single_grain(xm: np.array, ym: np.array, config: dict) -> np.array:
+def single_grain(xm: np.array, ym: np.array, config: dict, eta_i: int) -> np.array:
 
     radius: np.array = np.sqrt(xm**2 + ym**2) - config["initRadius"]
     radius = tanhmin(radius, config["interfaceWidth"])
@@ -13,5 +13,5 @@ def single_grain(xm: np.array, ym: np.array, config: dict) -> np.array:
     return radius * config["initEta"]
 
 
-def load_from_file(xm, ym, config):
+def load_from_file(xm, ym, config, eta_i):
     pass
