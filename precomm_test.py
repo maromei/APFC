ef84acfba69d37502ba19800cc858645ff9c01f3
maromei <max@pop-os.localdomain>
2023-01-08 13:54:56 +0100

    precommit test

---
+++ precomm_test.py
@@ -0,0 +1,8 @@
+import os
+
+import numpy as np
+
+
+import sys
+
+thisisareallya_longline = ["hallothisiaanotherreallylonglienm anasdf", "hallothisiaanotherreallylonglienm anasdf", "hallothisiaanotherreallylonglienm anasdf", "hallothisiaanotherreallylonglienm anasdf"]
