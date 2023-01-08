67fbd4fda047e3dae611a5c2244ee9bfe90c8b87
maromei <max@pop-os.localdomain>
2023-01-08 13:57:15 +0100

    precommit test 3

--- precomm_test.py
+++ precomm_test.py
@@ -1,8 +1,13 @@
 import os
+import sys
 
 import numpy as np
 
+thisisareallya_longline = [
+    "hallothisiaanotherreallylonglienm anasdf",
+    "hallothisiaanotherreallylonglienm anasdf",
+    "hallothisiaanotherreallylonglienm anasdf",
+    "hallothisiaanotherreallylonglienm anasdf",
+]
 
-import sys
-
-thisisareallya_longline = ["hallothisiaanotherreallylonglienm anasdf", "hallothisiaanotherreallylonglienm anasdf", "hallothisiaanotherreallylonglienm anasdf", "hallothisiaanotherreallylonglienm anasdf"]
+this_test = "hello"
