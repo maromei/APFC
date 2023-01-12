60dfdb1f0c99c7285ac06bdbdaf479581060dde8
maromei <max@pop-os.localdomain>
2023-01-12 12:00:53 +0100

    added utilss function to generate sim info string

--- src/python/sim/utils.py
+++ src/python/sim/utils.py
@@ -13,3 +13,25 @@ def make_path_arg_absolute(path: str):
         path = path[:-1]
 
     return "/".join([os.getcwd(), path])
+
+
+def build_sim_info_str(config, index):
+
+    txt = f"""\
+        \\begin{{center}}
+        sim iteration: {index} \\vspace{{0.5em}}
+        $B_x = {config['Bx']:.4f}, n_0 = {config['n0']:.4f}$
+        $v = {config['v']:.4f}, t = {config['t']:.4f}$
+        $\\Delta B_0 = {config['dB0']:.4f}$
+        $\\mathrm{{d}}t = {config['dt']:.4f}$ \\vspace{{0.5em}}
+        initial Radius: {config['initRadius']:.4f}
+        initial Eta in solid: {config['initEta']:.4f}
+        interface width: {config['interfaceWidth']:.4f}
+        domain: $[-{config['xlim']}, {config['xlim']}]^2$
+        points: {config['numPts']} x {config['numPts']}
+        \\end{{center}}
+    """
+
+    txt = "".join(map(str.lstrip, txt.splitlines(1)))
+
+    return txt
