bd6c031d7599209af5ca89af3bc41167b1dc7ea1
maromei <max@pop-os.localdomain>
2023-03-09 18:22:59 +0100

    added src to path in doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("../../src"))
+
 project = "APFC"
 copyright = "2022, maromei"
 author = "maromei"
