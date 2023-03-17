ac34365fdc3cd0617d61a0f32fa880c1bc35b9fa
maromei <max@pop-os.localdomain>
2023-03-17 17:40:30 +0100

    fixed loading eta file shape

--- src/sim/runners/fft_n0.py
+++ src/sim/runners/fft_n0.py
@@ -257,7 +257,7 @@ class FFTN0Sim:
 
         for eta_i in range(self.eta_count):
             self.etas[eta_i, :, :] += initialize.load_eta_from_file(
-                shape, config, eta_i
+                self.xm.shape, config, eta_i
             )
 
     #########################
